80f3f6fdb6a2f6c0ad6a3cd423de95655feb8b7e
Suin Hong <78659190+ssuinh@users.noreply.github.com>
2024-09-03 17:31:23 +0900

    [Silver I] Title: IOIOI, Time: 108 ms, Memory: 113120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,19 +1,17 @@
 n = int(input())
 m = int(input())
-a = list(input())
-result = 0
+a = input()
+i, result, count = 0,0,0
 
-for i in range(m-(2*n+1)+1):
-    if a[i] == 'I':
-        for j in range(2*n+1):
-            if j == 0:
-                pre = a[i]
-            else:
-                if a[i+j] != pre:
-                    pre=a[i+j]
-                    continue
-                else:
-                    break
-        else:
+while i < (m-1):
+    if a[i: i+3] == 'IOI':
+        count +=1
+        i +=2
+        if count == n:
             result += 1
+            count -=1
+    else:
+        i +=1
+        count = 0
+
 print(result)
