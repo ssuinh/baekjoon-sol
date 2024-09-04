a6da6f418fc7a2ecf5e2a4a2df41f05e5182ad38
Suin Hong <78659190+ssuinh@users.noreply.github.com>
2024-09-04 20:04:21 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 276 ms, Memory: 172700 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,32 +1,29 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-def bfs(start):
-    Q = deque()
-    Q.append(start)
-    visited[start] = 1   # 방문 표시
+def dfs(v):
+    global visited
+    if visited[v] == 0:
+        visited[v] = 1
 
-    while Q:
-        val = Q.popleft()
-        for node in graph[val]:
-            if visited[node] == 0:
-                Q.append(node)
-                visited[node] = 1
-
+    for w in graph[v]:
+        if visited[w] == 0:
+            visited[w] = 1
+            dfs(w)
 
 n, m = map(int, input().split())
 graph = [[] for _ in range(n+1)]
-visited = [0]*(n+1)
-result = 0
+visited = [0] * (n+1)
+visited[0] = 1
+cnt = 0
 
 for i in range(m):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+    start, end = map(int, input().split())
+    graph[start].append(end)
+    graph[end].append(start)
 
-for i in range(1, n+1):
-    if visited[i] == 0:  # 방문하지 않은 노드가 있다면 bfs 호출
-        bfs(i)
-        result += 1
-print(result)
+while visited.count(0) > 0:
+    cnt += 1
+    dfs(visited.index(0))
+
+print(cnt)
