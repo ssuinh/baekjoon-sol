7304a3cc76164424537593f1147a0cbdf9a5c1ec
HongSuin <78659190+ssuinh@users.noreply.github.com>
2025-01-04 22:25:51 +0900

    [Silver III] Title: 수열, Time: 92 ms, Memory: 41676 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,16 +1,23 @@
+# 슬라이딩 방식 아닌 메모이제이션 DP 접근법
+
 import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-arr = list(map(int, input().split()))
-sum_val = sum(arr[:k])
-pre_idx = 0
-max_val = sum_val
+lst = list(map(int, input().split()))
 
-for i in range(n-k):
-    sum_val = sum_val - arr[pre_idx] + arr[i+k]
-    pre_idx += 1
-    if sum_val >= max_val:
-        max_val = sum_val
+# 틀린이유 : prefix의 길이는 맨 앞 0이 있어야함. k=1일 경우 뺄 이전 요소가 없기 때문에 runtimeerror 발생.
+prefix = [0]   # 수열 요소간 차
+tmp = 0
+for i in lst:
+    tmp += i
+    prefix.append(tmp)
 
-print(max_val)
+ans = []
+temp = 0
+for i in range(k, n+1):   
+    # index 4까지의 누적합 - 2까지의 누적합 == (2+1)~4 요소의 누적합
+    temp = prefix[i] - prefix[i-k]
+    ans.append(temp)  # k=2 두개 요소들의 합을 저장한 리스트
+
+print(max(ans))
