a8dc8849c9d92de7202896015500478084470852
HongSuin <78659190+ssuinh@users.noreply.github.com>
2025-01-23 14:29:39 +0900

    Update 토마토.py

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -23,7 +23,6 @@ def bfs():
                     graph[ni][nj][nk] = graph[r][c][v] + 1
 
 m, n, h = map(int, input().split())
-
 graph = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
 
 flag = False
@@ -32,20 +31,22 @@ Q = deque()
 for i in range(h):
     for j in range(n):
         for k in range(m):
-            if graph[i][j][k] == 1:  # 방문하지 않고 익은 토마토가 있는 곳이라면 bfs 호출
-                Q.append((i, j, k))
-                graph[i][j][k] = 2
+            # 익은 토마토 주위 토마토가 먼저 익어야함으로 익은 토마토 좌표를 큐에 먼저 다 넣어야함.
+            if graph[i][j][k] == 1:
+                    Q.append((i, j, k))
+                    graph[i][j][k] = 2
 bfs()
 
 val = 0
+# 탐색 끝난 그래프 순회하여 값 판단.
 for i in graph:
     for j in i:
-        if 0 in j:
+        if 0 in j:   # 안익은 토마토 존재
             flag = True
-        else:
+        else:    # 안익은 토마토 없음. 최소 날짜 저장.
             val = max(max(j), val)
 
-if flag:
+if flag:   # 안익은 토미토 존재
     print(-1)
-else:
+else:    # 안익은 토마토 없음. 모두 익히는데 필요한 최소 날짜 출력.
     print(val-2)
