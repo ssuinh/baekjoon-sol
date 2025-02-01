5db691a2dedf645b317bc12dbfe3f851a0c65623
HongSuin <78659190+ssuinh@users.noreply.github.com>
2025-02-01 19:01:33 +0900

    [Silver II] Title: 스택 수열, Time: 772 ms, Memory: 37380 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,31 +1,28 @@
-lst = []  # input
-stack = []
-result = []
+import sys
+input = sys.stdin.readline
+from collections import deque
 
-idx = 0   # 입력 받은 수열 lst의 idx를 한칸씩 옮기면 굳이 pop안해도 됨.(reversed도 안해도 됨)
 n = int(input())
-for i in range(n):
-    a = int(input())
-    lst.append(a)
+ans = []
+result = ''
+cnt = 0
+Q = deque()
 
 for i in range(n):
-    stack.append(i+1)
-    result.append('+')
+    tmp = int(input())
+    Q.append(tmp)
 
-    
-    while True:
-        if len(stack) != 0 and len(lst) != 0:
-            if stack[-1] == lst[idx]:
-                stack.pop()
-                idx+=1   # 지우면서 다음 수열 요소인지 판별하기
-                result.append('-')
-            else:
-                break
-        else:
-            break
+for i in range(1, n+1):
+    ans.append(i)
+    result += '+'
 
-if len(stack) != 0:
-    print("NO")
+    while ans and ans[-1] == Q[0]:
+        Q.popleft()
+        ans.pop()
+        result += '-'
+
+if Q:
+    print('NO')
 else:
     for i in result:
         print(i)
